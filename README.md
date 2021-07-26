91ef0147b812185661e4386537a9743a24bf7fcf
Victor Augusto <victor.augusto60@gmail.com>
2021-07-26 15:48:46 -0300

    Acessado tutorial Path Parameters

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Instruções da documentação de FastAPI utilizando Python.
 ![image](https://user-images.githubusercontent.com/48570599/127035094-6e0a8958-e549-4f0f-9ba2-412b7121b9b1.png)
 
 Tutorial: https://fastapi.tiangolo.com/pt/
+
+Tutorial - Path Parameters: https://fastapi.tiangolo.com/pt/tutorial/path-params/
---
+++ path_Parameters/main.py
@@ -0,0 +1,23 @@
+from enum import Enum
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    victor = 'Victor Augusto'
+    larissa = 'Larissa Zandonadi'
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+
+@app.get('/models/{model_name}')
+async def get_model(model_name: ModelName):
+    if model_name.value == ModelName.victor:
+        return {'model_name': model_name, 'message': 'Acessado Victor A.'}
+    if model_name == ModelName.larissa:
+        return {'model_name': model_name, 'message': 'Acessado Larissa Z.'}
+    return {'model_name': model_name, 'message': 'Acessou nenhum.'}
