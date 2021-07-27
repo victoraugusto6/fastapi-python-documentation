147e75cc584ad8ad3e1676c3bb2aa099ef1a9660
Victor Augusto <victor.augusto60@gmail.com>
2021-07-26 21:06:49 -0300

    Criado teste async

---
+++ path_Parameters/tests/test_main.py
@@ -0,0 +1,28 @@
+import pytest
+from httpx import AsyncClient
+
+from path_Parameters.main import app, ModelName
+
+
+@pytest.mark.asyncio
+async def test_read_item(item_id=1):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get(f'/items/{item_id}')
+    assert response.status_code == 200
+    assert response.json() == {'item_id': 1}
+
+
+@pytest.mark.asyncio
+async def test_get_model_first(model_name=ModelName.victor):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get(f'/models/{model_name}')
+    assert response.status_code == 200
+    assert response.json() == {'model_name': model_name, 'message': 'Acessado Victor A.'}
+
+
+@pytest.mark.asyncio
+async def test_get_second(model_name=ModelName.larissa):
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get(f'/models/{model_name}')
+    assert response.status_code == 200
+    assert response.json() == {'model_name': model_name, 'message': 'Acessado Larissa Z.'}
