a78d458357981eb7b8414f5ef7e1640e817f6be6
Victor Augusto <victor.augusto60@gmail.com>
2021-07-26 23:38:23 -0300

    Criado testes para main_2.py - put

--- apis/query_Parameters/main.py
+++ apis/query_Parameters/main.py
@@ -6,9 +6,10 @@ app = FastAPI()
 fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}]
 
 
-# @app.get('/items/')
-# async def read_item(skip: int = 0, limit: int = 10):
-#     return fake_items_db[skip:skip + limit]
+@app.get('/items/')
+async def read_item_interval(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip:skip + limit]
+
 
 # @app.get('/items/')
 # async def read_item(item_id: str, q: Optional[str] = None):
